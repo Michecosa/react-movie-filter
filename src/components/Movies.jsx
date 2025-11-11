9b3edd45d36c877d3b10c8471589d9e7416b2c27
Michela <micheladellagatta1@gmail.com>
2025-11-11 11:50:47 +0100

    Add input for filtered search and useStates

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 export default function Movies({ moviesList }) {
   const [movies, setMovies] = useState(moviesList);
+  const [searchGenre, setSearchGenre] = useState("");
+  const [filteredGenre, setFilteredGenre] = useState([]);
 
   return (
-    <ul>
-      {movies.map((movie, index) => (
-        <li key={index}>
-          {movie.title} - {movie.genre}
-        </li>
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        placeholder="Ricerca per genere..."
+        value={searchGenre}
+        onChange={(e) => setSearchGenre(e.target.value)}
+      />
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>
+            {movie.title} - {movie.genre}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
