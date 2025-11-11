fda27847b2144ccc350fa1879c9b5eb8cec320aa
Michela <micheladellagatta1@gmail.com>
2025-11-11 12:39:52 +0100

    Add bootstrap classes to style the select section

--- src/components/movies-list/Movies.jsx
+++ src/components/movies-list/Movies.jsx
@@ -45,17 +45,20 @@ export default function Movies({ moviesList }) {
         setNewGenre={setNewGenre}
       />
 
-      <select
-        value={selectedGenre}
-        onChange={(e) => setSelectedGenre(e.target.value)}
-      >
-        <option value="">Tutti i generi</option>
-        {genres.map((genre, index) => (
-          <option key={index} value={genre}>
-            {genre}
-          </option>
-        ))}
-      </select>
+      <div className="container mt-4">
+        <select
+          className="form-select mb-3"
+          value={selectedGenre}
+          onChange={(e) => setSelectedGenre(e.target.value)}
+        >
+          <option value="">Tutti i generi</option>
+          {genres.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </div>
 
       <MoviesList filteredMovies={filteredMovies} />
     </>
