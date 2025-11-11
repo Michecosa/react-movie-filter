d0960fc8c9fd6f7661b4e9f5cff10f9c87f104eb
Michela <micheladellagatta1@gmail.com>
2025-11-11 12:37:47 +0100

    Add bootstrap classes to style the MoviesList component

--- src/components/movies-list/MoviesList.jsx
+++ src/components/movies-list/MoviesList.jsx
@@ -1,13 +1,24 @@
 export default function MoviesList({ filteredMovies }) {
   return (
-    <>
-      <ul>
-        {filteredMovies.map((movie, index) => (
-          <li key={index}>
-            {movie.title} - {movie.genre}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="container mt-4">
+      {filteredMovies.length === 0 ? (
+        <p className="text-center text-muted">Nessun film trovato</p>
+      ) : (
+        <div className="row g-3">
+          {filteredMovies.map((movie, index) => (
+            <div key={index} className="col-md-4">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p className="card-text">
+                    <span className="fw-semibold">Genere:</span> {movie.genre}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
